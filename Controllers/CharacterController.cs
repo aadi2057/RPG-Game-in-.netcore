8e893f067f0a67620d39f660b38efa79c1629e5a
Aaditya Jha <aadityajha2000@gmail.com>
2021-01-10 15:41:15 +0545

    Upto Automapper

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,27 +1,42 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Models;
+using dotnet_rpg.Service.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CharacterController: ControllerBase
+    public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character>{
-            new Character(),
-            new Character{Name = "Sam", Id =2}
-        };
+        
+        private readonly ICharacterService _characterService;
 
-        public IActionResult Get()
+        public CharacterController(ICharacterService characterService)
         {
-            return Ok(characters);
+            _characterService = characterService;
+
+        }
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetSingle(int id){
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCharacter(AddCharacterDto newCharacter)
+        {
+            
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
     }
 }
